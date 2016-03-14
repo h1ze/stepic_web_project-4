a87ecd38804e7688450c759946f2a50d792d9b34
h1ze <burgov@mail.ru>
2016-03-14 14:42:21 +0400

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,14 +1,9 @@
-from django.conf.urls import include, url
+
+from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = [
-    url(r'^$', 'qa.views.test'),
-    url(r'^login/.*$', 'qa.views.test'),
-    url(r'^signup/.*$', 'qa.views.test'),
-    url(r'^questions/\d+/$', 'qa.views.test'),
-    url(r'^ask/.*$', 'qa.views.test'),
-    url(r'^popular/.*$', 'qa.views.test'),
-    url(r'^new/.*$', 'qa.views.test')
-]
+urlpatterns = patterns('',
+    url(r'^', include('qa.urls')),
+)
